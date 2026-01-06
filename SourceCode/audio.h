086b281b1f849cdf0e0a8eff8ddd0d70a0692093
陸生 小西 <2250198@ecc.ac.jp>
2026-01-06 17:03:27 +0900

    サウンドコード入れといた

---
+++ SourceCode/audio.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// XWBの種類
+#define XWB_SE1          0
+
+// SE1の効果音
+#define XWB_STAGE1_COIN     0
+#define XWB_STAGE1_JUMP     1
+#define XWB_STAGE1_SHOT     2
+#define XWB_STAGE1_MISSILE  3
+#define XWB_STAGE1_LASER    4
+
+// 関数のプロトタイプ宣言
+void audio_init();
+void audio_deinit();
