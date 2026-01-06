32cdb39139508e1b155dd0137591cad177de83d8
陸生 小西 <2250198@ecc.ac.jp>
2026-01-06 19:42:42 +0900

    オーディオ関連

--- SourceCode/audio.h
+++ SourceCode/audio.h
@@ -4,11 +4,11 @@
 #define XWB_SE1          0
 
 // SE1の効果音
-#define XWB_STAGE1_COIN     0
-#define XWB_STAGE1_JUMP     1
-#define XWB_STAGE1_SHOT     2
-#define XWB_STAGE1_MISSILE  3
-#define XWB_STAGE1_LASER    4
+#define XWB_SE1_COIN     0
+#define XWB_SE1_JUMP     1
+#define XWB_SE1_SHOT     2
+#define XWB_SE1_MISSILE  3
+#define XWB_SE1_LASER    4
 
 // 関数のプロトタイプ宣言
 void audio_init();
